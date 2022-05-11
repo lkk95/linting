648f549ae966964e449e62c9d4587d5687410c5d
Laura Kesel <laura.kesel@gmail.com>
2022-05-11 14:52:26 +0200

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+//Change color of bookmark
+const bookmark = document.querySelector(".bookmark");
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("filled");
+});
+
+//Show answer
+const button = document.querySelector(".card_button");
+const answer = document.querySelector(".hidden");
+
+button.addEventListener("click", () => {
+  if (button.textContent === "Show Answer") {
+    button.textContent = "Hide Answer";
+  } else {
+    button.textContent = "Show Answer";
+  }
+  answer.classList.toggle("hidden");
+  //this works but not chnages back afterwards
+});
